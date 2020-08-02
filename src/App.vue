b214efa850d18163a535d2d97aa8ffa2ca861d00
Greyun <yhy3587@gmail.com>
2020-08-02 15:04:08 +0900

    add todo components

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,24 @@
 <template>
-
+  <div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput></TodoInput>
+    <TodoList></TodoList>
+    <TodoFooter></TodoFooter>
+  </div>
 </template>
 
 <script>
+  import TodoHeader from './components/TodoHeader'
+  import TodoInput from './components/TodoInput'
+  import TodoList from './components/TodoList'
+  import TodoFooter from './components/TodoFooter'
 export default {
-
+  components: {
+    TodoHeader,
+    TodoInput,
+    TodoList,
+    TodoFooter
+  }
 }
 </script>
 
