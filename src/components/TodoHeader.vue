d83de75dd12a47b36aa3a3b0050fe3a58ab0684b
Greyun <yhy3587@gmail.com>
2020-08-02 15:29:49 +0900

    add style in App and TodoHeader

--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -1,7 +1,7 @@
 <template>
-    <div>
-        Header
-    </div>
+    <header>
+        <h1>Simple TODO App!</h1>
+    </header>
 </template>
 
 <script>
@@ -11,5 +11,9 @@
 </script>
 
 <style scoped>
-
+    h1 {
+        color: #2F3B52;
+        font-weight: 900;
+        margin: 2.5rem 0 1.5rem;
+    }
 </style>
