4347c8e7298b77ee53819f6475e814d65a63335b
Greyun <yhy3587@gmail.com>
2020-08-02 16:14:16 +0900

    add input data int localStorage from todo input

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,12 +1,24 @@
 <template>
   <div>
-    Input
+    <input type="text" v-model="newTodoItem">
+    <button v-on:click="addTodo">add</button>
   </div>
 </template>
 
 <script>
   export default {
-    name: "TodoInput"
+    name: "TodoInput",
+    data() {
+      return {
+        newTodoItem: ""
+      }
+    },
+    methods: {
+      addTodo() {
+        localStorage.setItem('newTodoItem', this.newTodoItem);
+        this.newTodoItem = "";
+      }
+    }
   }
 </script>
 
